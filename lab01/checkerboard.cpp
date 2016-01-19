c889ef4cbe7db95ece79010dec459c5f76ea1986
Ashley Sattler <asattle2@vols.utk.edu>
2016-01-19 17:14:58 -0500

    lab 1 error checking

--- lab01/checkerboard.cpp
+++ lab01/checkerboard.cpp
@@ -12,7 +12,23 @@ int main() {
 	int rows, columns, cycleSize, width = 0;
 	char startingCharacter, printChar;
 
-	cin >> rows >> columns >> startingCharacter >> cycleSize >> width;
+	// User needs to input 2 ints, a single char, and 2 ints
+	if (!(cin >> rows >> columns >> startingCharacter >> cycleSize >> width)) {
+		cout << "Invalid arguments given. ";
+		cout << "Please enter 2 ints, a character, and 2 ints.\n";
+		return 1;
+	}
+
+	// We will process the input and check for errors before progressing.
+	if (rows <= 0 || columns <= 0 || cycleSize <= 0 || width <= 0) {
+		cout << "Cannot use zero or negative numbers.\n";
+		return 2;
+	}
+	if (startingCharacter + cycleSize > 127) {
+		cout << "Invalid input. ";
+		cout << "Please enter a different character or a smaller cycle size.\n";
+		return 3;
+	}
 
 	for (int r = 0; r < rows; r++) {
 		for (int wr = 0; wr < width; wr++) {
