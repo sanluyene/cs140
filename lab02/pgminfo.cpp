51365ad9785256f54ade3f4a62ad8ebe2336911b
Ashley Sattler <asattler@iiaweb.com>
2016-02-03 12:23:52 -0500

    lab 2 pt 1 fixed

--- lab02/pgminfo.cpp
+++ lab02/pgminfo.cpp
@@ -1,7 +1,7 @@
 // CS140 Spring 2016
 // Lab 02: Reading PGM files
 // Author: Ashley Sattler
-// Date: 02/01/2016
+// Date: 02/03/2016
 
 #include <iostream>
 #include <vector>
@@ -11,22 +11,10 @@
 
 using namespace std;
 
-class Pixel {
-	public:
-		int red, green, blue;
-
-		// This is a 0-argument constructor class for Pixel
-		Pixel();
-
-		// This is a 3-argument constructor class for Pixel that will set the 3 struct variables
-		// to the user-defined ints
-		Pixel(int r, int g, int b);
-};
-
 class PGMFile {
 	public: 
-		vector<Pixel> pixv;
-		int width, height, color, numPixels;
+		vector<int> pixels;
+		int column, row, color, numPixels;
 		string p2;
 
 		// This is a 0-argument constructor class for PGMFile
@@ -35,9 +23,8 @@ class PGMFile {
 
 int main(int argc, char **argv) {
 	PGMFile pgm;
-	bool goodFile;
-	double avgPixel = 0, tempTotal = 0;
-	int r = 0, g = 0, b = 0, i = 0, currentPixel = 0;
+	double avgPixel = 0;
+	int i = 1, currentPixel = -1, tempNumber = 0;
 
 	cin >> pgm.p2;
 	// First string should be P2
@@ -45,70 +32,59 @@ int main(int argc, char **argv) {
 		fprintf(stderr, "Bad PGM file -- first word is not P2\n");
 		return -1;
 	}
+	//printf("P2? %s\n",pgm.p2.c_str());
+
+	cin >> pgm.column;
+	if (pgm.column < 0) {
+		fprintf(stderr, "Bad PGM file -- No column specification\n");
+		return -1;
+	}
+	//printf("column? %d\n",pgm.column);
+
+	cin >> pgm.row;
+	if (pgm.row < 0) {
+		fprintf(stderr, "Bad PGM file -- No row specification\n");
+		return -1;
+	}
+	//printf("row? %d\n",pgm.row);
+
+	cin >> pgm.color;
+	if (pgm.color != 255) {
+		fprintf(stderr, "Bad PGM file -- 255 missing\n");
+		return -1;
+	}
+	//printf("color? %d\n",pgm.color);
 
 	while(!cin.eof()) {
-		if (i > 0 && cin.fail()) break;
-		else if (i == 1) {
-			pgm.width = tempNumber;
-			cout << sin << endl;
-			if (pgm.width < 0) {
-				fprintf(stderr, "Bad PGM file -- No column specification\n");
-				return -1;
-			}
+		// Copy all the pixels
+		if (tempNumber < 0 || tempNumber > 255) {
+			fprintf(stderr, "Bad PGM file -- pixel %d is not a number between 0 and 255\n", currentPixel);
+			return -1;
 		}
-		else if (i == 2) {
-			pgm.height = tempNumber;
-			if (pgm.height < 0) {
-				fprintf(stderr, "Bad PGM file -- No row specification\n");
-				return -1;
-			}
-		}
-		else if (i == 3) {
-			pgm.color = tempNumber;
-			if (pgm.color != 255) {
-				fprintf(stderr, "Bad PGM file -- 255 missing\n");
-				return -1;
-			}
-		}
-		else if (i >= 3) { // Copy all the pixels
-			while (!cin.eof) {
-			int pixNum = 0;
-			if (r < 0 || r > 255 || g < 0 || g > 255 || b < 0 || b > 255) {
-				fprintf(stderr, "Bad PGM file -- pixel %d is not a number between 0 and 255\n", currentPixel);
-				return -1;
-			}
-			}
-			pgm.pixv.push_back(Pixel(r, g, b));
-			currentPixel++;
-		}
-		i++;
+		pgm.pixels.push_back(tempNumber);
+		currentPixel++;
 		cin >> tempNumber;
 	}
 
-	pgm.numPixels = pgm.height * pgm.width;
+	pgm.numPixels = pgm.row * pgm.column;
+	if (pgm.numPixels != pgm.pixels.size()) {
+		fprintf(stderr, "Bad PGM file -- Extra stuff after the pixels");
+		return -1;
+	}
 
 	// Calculate the average pixel number
-	for (int i = 0; i < pgm.pixv.size(); i++) {
-		tempTotal += pgm.pixv[i].red;
-		tempTotal += pgm.pixv[i].green;
-		tempTotal += pgm.pixv[i].blue;
-		tempTotal /= 3;
-		avgPixel += tempTotal;
+	for (i = 0; i < pgm.pixels.size(); i++) {
+		avgPixel += pgm.pixels[i];
 	}
-	avgPixel /= pgm.pixv.size();
+	avgPixel /= pgm.numPixels;
 
-	printf("# Rows:%-11s\n", pgm.height);
-	printf("# Columns:%-8s\n", pgm.width);
-	printf("# Pixels:%-9s\n", pgm.pixv.size());
-	printf("Avg Pixel:%-8s\n", avgPixel);
+	printf("# Rows: %11d\n", pgm.row);
+    printf("# Columns: %8d\n", pgm.column);
+    printf("# Pixels: %9d\n", pgm.numPixels);
+    printf("Avg Pixel: %8.3f\n", avgPixel);
 
 	return 0;
 }
 
-Pixel::Pixel(){}
-
-Pixel::Pixel(int r, int g, int b)
-	: red(r), green(g), blue(b) {}
-
 PGMFile::PGMFile()
-	: width(0), height(0), color(0), numPixels(0) {}
+	: column(0), row(0), color(0), numPixels(0) {}
