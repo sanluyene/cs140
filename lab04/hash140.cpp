d833945e13e6895b9102b0b04ffc1abcea2bf7dd
Ash <thesion@gmail.com>
2016-02-13 16:00:19 -0500

    Create hash140.cpp
    beginning of lab4 groundwork

---
+++ lab04/hash140.cpp
@@ -0,0 +1,41 @@
+// CS140 Spring 2016
+// Lab 04: Playing with hash tables
+// Author: Ashley Sattler
+// Date: 02/13/2016
+
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+HashTable(int table_size, string function, string collision) {
+
+}
+
+void HashTable::Add_Hash(string &key, string &val) {
+	// % 0xkey to find index
+	// HashTable[index] = val
+	//if HashTable[index] != ""
+	// then collision technique
+
+	//fprintf("Hash Table Full");
+	//fprintf("Couldn't put %s into the table", key);
+}
+
+string HashTable::Find(string &key) {
+
+	return "";
+}
+
+void HashTable::Print() {
+
+}
+
+int HashTable::Total_Probes() {
+
+	return 0;
+}
