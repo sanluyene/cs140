6c9bb27d84d1b99039d773ad4e1b12f31d827607
Ashley Sattler <asattle2@vols.utk.edu>
2016-03-02 08:14:51 -0500

    lab 6 starter things

---
+++ lab06/keno.cpp
@@ -0,0 +1,9 @@
+// CS140 Spring 2016
+// Lab 06: Keno
+// Author: Ashley Sattler
+// Date: 03/02/2016
+
+#include <iostream>
+
+using namespace std;
+
---
+++ lab06/product.cpp
@@ -0,0 +1,53 @@
+// CS140 Spring 2016
+// Lab 06: Keno
+// Author: Ashley Sattler
+// Date: 03/02/2016
+
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include "product.h"
+
+using namespace std;
+
+void Product::Invert() {
+
+}
+
+void Product::Multiply_Number(int n) {
+
+}
+
+void Product::Divide_Number(int n) {
+
+}
+
+void Product::Multiply_Factorial(int n) {
+
+}
+
+void Product::Divide_Factorial(int n) {
+
+}
+
+void Product::Multiply_Binom(int n, int k) {
+
+}
+
+void Product::Divide_Binom(int n, int k) {
+
+}
+
+void Product::Clear() {
+
+}
+
+void Product::Print() {
+
+}
+
+double Product::Calculate_Product() {
+	double i;
+	i = 0.0;
+	return i;
+}
