9d904facd355202f55c0fa18d645f48cc6ece117
Ashley Sattler <asattle2@vols.utk.edu>
2016-03-02 08:38:57 -0500

    lab 6 multiplynumber, dividenumber, invert

--- lab06/product.cpp
+++ lab06/product.cpp
@@ -11,15 +11,23 @@
 using namespace std;
 
 void Product::Invert() {
+	int tmp = 0;
 
+	for (int i = 0; i < numerator.size(); i++) {
+		tmp = numerator[i];
+		numerator[i] = denominator[i];
+		denominator[i] = tmp;
+	}		
 }
 
 void Product::Multiply_Number(int n) {
-
+	if (numerator.size() <= 0 || denominator.size() <= 0) return;
+	numerator[n]++;
 }
 
 void Product::Divide_Number(int n) {
-
+	if (numerator.size() <= 0 || denominator.size() <= 0) return;
+	denominator[n]++;
 }
 
 void Product::Multiply_Factorial(int n) {
