9baa02422f47cdc73f40689642635f77736d4605
Ashley Sattler <asattle2@vols.utk.edu>
2016-03-09 09:24:10 -0500

    lab 7 starter

---
+++ lab07/code_processor.cpp
@@ -0,0 +1,71 @@
+#include <set>
+#include <deque>
+#include <map>
+#include <vector>
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include "code_processor.h"
+
+using namespace std;
+
+int Code_Processor::New_Prize(string id, string description, int points, int quantity) {
+
+	return 0;
+}
+
+int Code_Processor::New_User(string username, string realname, int starting_points) {
+
+	return 0;
+}
+
+int Code_Processor::Delete_User(string username) {
+
+	return 0;
+}
+
+int Code_Processor::Add_Phone(string username, string phone) {
+
+	return 0;
+}
+
+int Code_Processor::Remove_Phone(string username, string phone) {
+
+	return 0;
+}
+
+string Code_Processor::Show_Phones(string username) {
+
+	return "";
+}
+
+int Code_Processor::Enter_Code(string username, string code) {
+
+	return 0;
+}
+
+int Code_Processor::Text_Code(string phone, string code) {
+
+	return 0;
+}
+
+int Code_Processor::Mark_Code_Used(string code) {
+
+	return 0;
+}
+
+int Code_Processor::Balance(string username) {
+
+	return 0;
+}
+
+int Code_Processor::Redeem_Prize(string username, string prize) {
+
+	return 0;
+}
+
+int Code_Processor::Write(const char *file) {
+
+	return 0;
+}
+
