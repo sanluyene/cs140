449025312d5862dec56df83809c2084ea9e6158a
Ashley Sattler <asattle2@vols.utk.edu>
2016-03-14 15:09:07 -0400

    lab 7 New_Prize New_User Balance 1/120

--- lab07/code_processor.cpp
+++ lab07/code_processor.cpp
@@ -10,11 +10,35 @@
 using namespace std;
 
 int Code_Processor::New_Prize(string id, string description, int points, int quantity) {
+	Prize *p;
+	map <string, Prize *>::iterator pit;
+
+	if (points < 0 || quantity < 0) return -1;
+	pit = Prizes.find(id);
+	if (pit == Prizes.end()) {
+		p = new Prize;
+		p->id = id;
+		p->description = description;
+		p->points = points;
+		p->quantity = quantity;
+	}
 
 	return 0;
 }
 
 int Code_Processor::New_User(string username, string realname, int starting_points) {
+	User *u;
+	map <string, User *>::iterator uit;
+
+	if (starting_points < 0) return -1;
+	uit = Names.find(username);
+	if (uit == Names.end()) {
+		u = new User;
+		u->username = username;
+		u->realname = realname;
+		u->points = starting_points;
+	}
+	else return -1;
 
 	return 0;
 }
@@ -55,8 +79,10 @@ int Code_Processor::Mark_Code_Used(string code) {
 }
 
 int Code_Processor::Balance(string username) {
-
-	return 0;
+	map <string, User *>::iterator uit;
+	uit = Names.find(username);
+    if (uit == Names.end()) return -1;
+	else return uit->second->points;
 }
 
 int Code_Processor::Redeem_Prize(string username, string prize) {
