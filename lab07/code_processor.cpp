cd2cf7d30363b102ba4e3d57874993de65a71dc2
Ashley Sattler <asattle2@vols.utk.edu>
2016-03-14 16:29:21 -0400

    lab 7 write testing 3/120

--- lab07/code_processor.cpp
+++ lab07/code_processor.cpp
@@ -21,6 +21,7 @@ int Code_Processor::New_Prize(string id, string description, int points, int qua
 		p->description = description;
 		p->points = points;
 		p->quantity = quantity;
+		Prizes[id] = p;
 	}
 
 	return 0;
@@ -37,6 +38,7 @@ int Code_Processor::New_User(string username, string realname, int starting_poin
 		u->username = username;
 		u->realname = realname;
 		u->points = starting_points;
+		Names[username] = u;
 	}
 	else return -1;
 
@@ -49,6 +51,16 @@ int Code_Processor::Delete_User(string username) {
 }
 
 int Code_Processor::Add_Phone(string username, string phone) {
+	map <string, User *>::iterator uit;
+
+	uit = Phones.find(phone);
+	if (uit != Phones.end()) return -1;
+    uit = Names.find(username);
+    if (uit == Names.end()) return -1;
+	else {
+		uit->second->phone_numbers.insert(phone);
+		Phones[phone] = uit->second;
+	}
 
 	return 0;
 }
@@ -80,8 +92,10 @@ int Code_Processor::Mark_Code_Used(string code) {
 
 int Code_Processor::Balance(string username) {
 	map <string, User *>::iterator uit;
+
 	uit = Names.find(username);
-    if (uit == Names.end()) return -1;
+
+	if (uit == Names.end()) return -1;
 	else return uit->second->points;
 }
 
@@ -93,6 +107,24 @@ int Code_Processor::Redeem_Prize(string username, string prize) {
 Code_Processor::~Code_Processor() {}
 
 int Code_Processor::Write(const char *file) {
+	ofstream fout;
+	FILE * code;
+	map <string, Prize *>::iterator pit;
+	map <string, User *>::iterator uit;
+
+	code = fopen(file, "w");
+
+	for (pit = Prizes.begin(); pit != Prizes.end(); pit++) {
+		fprintf(code, "PRIZE %s %s ", pit->first.c_str(), pit->second->description.c_str());
+		fprintf(code, "%d %d\n", pit->second->points, pit->second->quantity);
+	}
+	for (uit = Names.begin(); uit != Names.end(); uit++) {
+		fprintf(code, "ADD_USER %s %s %d\n", uit->first.c_str(), uit->second->realname.c_str(), uit->second->points);
+	}
+
+	for (uit = Phones.begin(); uit != Phones.end(); uit++) {
+		fprintf(code, "ADD_PHONE %s %s\n", uit->second->username.c_str(), uit->first.c_str());
+	}
 
 	return 0;
 }
