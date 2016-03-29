a6092b188b2f5ba158d38baf525ae7df3ba6ce48
Ashley Sattler <asattler@iiaweb.com>
2016-03-29 16:34:23 -0400

    lab 8 empty .cpp files

---
+++ lab08/dl_hash.cpp
@@ -0,0 +1,34 @@
+// CS140 Spring 2016
+// Lab 08: Dlisting
+// Author: Ashley Sattler
+// Date: 03/29/2016
+
+#include "dl_hash.h"
+
+DL_Hash::DL_Hash(int size) {
+
+}
+
+DL_Hash::~DL_Hash() {
+
+}
+
+void DL_Hash::Insert(string &s) {
+
+}
+
+int DL_Hash::Present(string &s) {
+
+}
+
+void DL_Hash::Erase(string &s) {
+
+}
+
+void DL_Hash::Strip_All_Substring(string &s) {
+
+}
+
+void DL_Hash::Print() {
+
+}
---
+++ lab08/dlist.cpp
@@ -0,0 +1,76 @@
+// CS140 Spring 2016
+// Lab 08: Dlisting
+// Author: Ashley Sattler
+// Date: 03/29/2016
+
+#include <iostream>
+#include <cstdlib>
+#include "dlist.h"
+
+Dlist::Dlist() {
+
+}
+
+Dlist::~Dlist() {
+
+}
+
+int Dlist::Empty() {
+
+}
+
+int Dlist::Size() {
+
+}
+
+void Dlist::Push_Front(string s) {
+
+}
+
+void Dlist::Push_Back(string s) {
+
+}
+
+string Dlist::Pop_Front() {
+	return "";
+}
+
+string Dlist::Pop_Back() {
+	return "";
+}
+
+Dnode *Dlist::Begin() {
+	Dnode *d;
+	d = new Dnode;
+	return d;
+}
+
+Dnode *Dlist::End() {
+	Dnode *d;
+	d = new Dnode;
+	return d;
+}
+
+Dnode *Dlist::Rbegin() {
+	Dnode *d;
+	d = new Dnode;
+	return d;
+}
+
+Dnode *Dlist::Rend() {
+	Dnode *d;
+	d = new Dnode;
+	return d;
+}
+
+void Dlist::Insert_Before(string s, Dnode *n) {
+
+}
+
+void Dlist::Insert_After(string s, Dnode *n) {
+
+}
+
+void Dlist::Erase(Dnode *n) {
+
+}
