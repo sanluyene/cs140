e724a3b4bae5a151499b96e3aa9fc034fb3377f0
Ashley Sattler <asattle2@vols.utk.edu>
2016-04-01 15:09:12 -0400

    lab 8 started working on dl_hash.cpp 65/100

--- lab08/dl_hash.cpp
+++ lab08/dl_hash.cpp
@@ -3,26 +3,71 @@
 // Author: Ashley Sattler
 // Date: 03/29/2016
 
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <string>
+#include <cstdio>
 #include "dl_hash.h"
 
-DL_Hash::DL_Hash(int size) {
+unsigned int djb_hash(string &s)
+{
+	int i;
+	unsigned int h;
 
+	h = 5381;
+
+	for (i = 0; i < s.size(); i++) {
+		h = (h << 5) + h + s[i];
+	}
+	return h;
+}
+
+DL_Hash::DL_Hash(int size) {
+	Dlist *newlist;
+
+	table.resize(size);
+
+	for (int i = 0; i < size; i++) {
+		newlist = new Dlist;
+		table[i] = newlist;
+	}
 }
 
 DL_Hash::~DL_Hash() {
+	for (int i = 0; i < table.size(); i++) {
 
+	}
 }
 
 void DL_Hash::Insert(string &s) {
+	unsigned int val;
 
+	val = djb_hash(s) % table.size();
+
+	table[val]->Push_Front(s);
 }
 
 int DL_Hash::Present(string &s) {
+	unsigned int val;
+	Dnode *d;
 
+	val = djb_hash(s) % table.size();
+	d = new Dnode;
+
+	d = table[val]->Begin();
+	while (d != table[val]->End()) {
+		if (d->s == s) return 1;
+		d = d->flink;
+	}
+
+	return 0;
 }
 
 void DL_Hash::Erase(string &s) {
+	unsigned int val;
 
+	val = djb_hash(s) % table.size();
 }
 
 void DL_Hash::Strip_All_Substring(string &s) {
@@ -30,5 +75,15 @@ void DL_Hash::Strip_All_Substring(string &s) {
 }
 
 void DL_Hash::Print() {
+	Dnode *d;
 
+	d = new Dnode;
+
+	for (int i = 0; i < table.size(); i++) {
+		d = table[i]->Begin();
+		while (d != table[i]->End()) {
+			printf("%4d %s\n", i, d->s.c_str());
+			d = d->flink;
+		}
+	}
 }
