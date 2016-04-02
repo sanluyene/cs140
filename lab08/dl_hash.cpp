7aed428f96e9cf641093b05da78f0b6d150bdc3b
Ash <thesion@gmail.com>
2016-04-02 16:46:38 -0400

    lab 8 added comments

--- lab08/dl_hash.cpp
+++ lab08/dl_hash.cpp
@@ -1,5 +1,5 @@
 // CS140 Spring 2016
-// Lab 08: Dlisting
+// Lab 08: Hash table for Dlists
 // Author: Ashley Sattler
 // Date: 03/29/2016
 
@@ -10,6 +10,7 @@
 #include <cstdio>
 #include "dl_hash.h"
 
+// This is our hashing function
 unsigned int djb_hash(string &s)
 {
 	int i;
@@ -23,31 +24,49 @@ unsigned int djb_hash(string &s)
 	return h;
 }
 
+// This is a constructor for the hash table
 DL_Hash::DL_Hash(int size) {
 	Dlist *newlist;
 
 	table.resize(size);
 
+	// We will be using separate chaining, so the vector must
+	// be properly sized and initially populated with Dlists
 	for (int i = 0; i < size; i++) {
 		newlist = new Dlist;
 		table[i] = newlist;
 	}
 }
 
+// This is a deconstructor for the hash table
 DL_Hash::~DL_Hash() {
-	for (int i = 0; i < table.size(); i++) {
+	Dnode *d, *next;
 
+	d = new Dnode;
+	next = new Dnode;
+
+	for (int i = 0; i < table.size(); i++) {
+		d = table[i]->Begin();
+		while (d != table[i]->End()) {
+			next = d->flink;
+			table[i]->Erase(d);
+			d = next;
+		}
 	}
 }
 
+// This method inserts values into the hash table
 void DL_Hash::Insert(string &s) {
 	unsigned int val;
 
 	val = djb_hash(s) % table.size();
 
+	// We only want to insert a value if it is not already in
+	// the table
 	if (!(Present(s))) table[val]->Push_Back(s);
 }
 
+// This method checks to see if a given value is in the hash table
 int DL_Hash::Present(string &s) {
 	unsigned int val;
 	Dnode *d;
@@ -64,6 +83,7 @@ int DL_Hash::Present(string &s) {
 	return 0;
 }
 
+// This method deletes a given value from the hash table
 void DL_Hash::Erase(string &s) {
 	unsigned int val;
 	Dnode *d;
@@ -71,16 +91,21 @@ void DL_Hash::Erase(string &s) {
 	val = djb_hash(s) % table.size();
 	d = new Dnode;
 
-	d = table[val]->Begin();
-	while (d != table[val]->End()) {
-		if (d->s == s) {
-			table[val]->Erase(d);
-			break;
+	// We only want to search if we know the value is in the table
+	if (Present(s)) {
+		d = table[val]->Begin();
+		while (d != table[val]->End()) {
+			if (d->s == s) {
+				table[val]->Erase(d);
+				break;
+			}
+			d = d->flink;
 		}
-		d = d->flink;
 	}
 }
 
+// This method will remove all hash table entries that contain the
+// given string
 void DL_Hash::Strip_All_Substring(string &s) {
 	Dnode *d;
 
@@ -97,6 +122,7 @@ void DL_Hash::Strip_All_Substring(string &s) {
 	}
 }
 
+// This method prints all values in the hash table
 void DL_Hash::Print() {
 	Dnode *d;
 
--- lab08/dlist.cpp
+++ lab08/dlist.cpp
@@ -1,5 +1,5 @@
 // CS140 Spring 2016
-// Lab 08: Dlisting
+// Lab 08: Double-Linked Lists practice
 // Author: Ashley Sattler
 // Date: 03/29/2016
 
@@ -7,6 +7,7 @@
 #include <cstdlib>
 #include "dlist.h"
 
+// This is the constuctor for our double-linked list
 Dlist::Dlist() {
 	Dnode *d;
 
@@ -19,6 +20,7 @@ Dlist::Dlist() {
 	size = 0;
 }
 
+// This is the deconstructor for our double-linked list
 Dlist::~Dlist() {
 	while(!(Empty())) {
 		Dnode *d, *next;
@@ -36,23 +38,28 @@ Dlist::~Dlist() {
 	}
 }
 
+// This method checks to see if the Dlist has any values in it
 int Dlist::Empty() {
 	if (size > 0) return 0;
 	return 1;
 }
 
+// This method returns the number of values in the Dlist
 int Dlist::Size() {
 	return size;
 }
 
+// This method inserts the given string into the front of the Dlist
 void Dlist::Push_Front(string s) {
 	Insert_Before(s, sentinel->flink);
 }
 
+// This method inserts a given string into the back of the Dlist
 void Dlist::Push_Back(string s) {
 	Insert_Before(s, sentinel);
 }
 
+// This method removes and returns the value at the front of the Dlist
 string Dlist::Pop_Front() {
 	string s = "";
 	Dnode *d, *next;
@@ -72,6 +79,7 @@ string Dlist::Pop_Front() {
 	return s;
 }
 
+// This method removes and returns the value at the back of the Dlist
 string Dlist::Pop_Back() {
 	string s = "";
 	Dnode *d, *prev;
@@ -91,22 +99,27 @@ string Dlist::Pop_Back() {
 	return s;
 }
 
+// This method returns the first item in the Dlist
 Dnode *Dlist::Begin() {
 	return sentinel->flink;
 }
 
+// This method returns the sentinel node
 Dnode *Dlist::End() {
 	return sentinel;
 }
 
+// This method returns the last item in the Dlist
 Dnode *Dlist::Rbegin() {
 	return sentinel->blink;
 }
 
+// This method returns the sentinel node
 Dnode *Dlist::Rend() {
 	return sentinel;
 }
 
+// This method inserts a given strings before the given element
 void Dlist::Insert_Before(string s, Dnode *n) {
 	Dnode *prev, *newnode;
 
@@ -123,10 +136,12 @@ void Dlist::Insert_Before(string s, Dnode *n) {
 	size++;
 }
 
+// This method inserts a given strings after the given element
 void Dlist::Insert_After(string s, Dnode *n) {
 	Insert_Before(s, n->flink);
 }
 
+// This method removes the given element
 void Dlist::Erase(Dnode *n) {
 	Dnode *prev, *next;
 
