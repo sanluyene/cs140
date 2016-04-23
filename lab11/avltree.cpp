89ee91e6819ad4f82d00b34e58f4aa8cf79878b7
Ash <thesion@gmail.com>
2016-04-23 10:34:33 -0400

    lab 11 balancing tweaks for AVL, comments, untested

--- lab11/avltree.cpp
+++ lab11/avltree.cpp
@@ -7,23 +7,29 @@
 #include <vector>
 #include <cstdlib>
 #include <cstdio>
+#include <cmath>
 #include "avltree.h"
 
 using namespace std;
 
+// Constructor for our new AVL Tree
 AVLTree::AVLTree() {
     sentinel = new AVLTNode;
     sentinel->parent = NULL;
     sentinel->left = NULL;
     sentinel->right = sentinel;
+    sentinel->height = -1;
     size = 0;
 }
 
+// Deconstructor for our AVL Tree
 AVLTree::~AVLTree() {
     recursive_destroy(sentinel->right);
     delete sentinel;
 }
 
+// This method will find the appropriate parent node for our new key
+// and insert it into our AVL Tree
 int AVLTree::Insert(string key, void *val) {
     AVLTNode *parent;
     AVLTNode *n;
@@ -55,9 +61,14 @@ int AVLTree::Insert(string key, void *val) {
         parent->right = n;
     }
     size++;
+    n->height = (n->left->height > n->right->height) ? (n->left->height + 1) : (n->right->height + 1);
+
+    Check_Balance(n->parent);
+
     return 1;
 }
 
+// This method will find the specified key in the AVL Tree if it exists
 void *AVLTree::Find(string key) {
     AVLTNode *n;
 
@@ -73,6 +84,8 @@ void *AVLTree::Find(string key) {
     }
 }
 
+// This method will delete the specified key in the AVL Tree if it exists
+// and rebalance our tree if necessary
 int AVLTree::Delete(string key) {
     AVLTNode *n, *parent, *mlc;
     string tmpkey;
@@ -95,7 +108,10 @@ int AVLTree::Delete(string key) {
         } else {
             parent->right = n->right;
         }
-        if (n->right != sentinel) n->right->parent = parent;
+        if (n->right != sentinel) {
+            n->right->parent = parent;
+            n->right->height--;
+        }
         delete n;
         size--;
     } else if (n->right == sentinel) {
@@ -105,6 +121,7 @@ int AVLTree::Delete(string key) {
             parent->right = n->left;
         }
         n->left->parent = parent;
+        n->left->height--;
         delete n;
         size--;
     } else {
@@ -114,22 +131,30 @@ int AVLTree::Delete(string key) {
         Delete(tmpkey);
         n->key = tmpkey;
         n->val = tmpval;
+        n->height--;
     }
+
+    Check_Balance(parent);
+
     return 1;
 }
 
+// This method prints our AVL Tree In Order
 void AVLTree::Print() {
     recursive_inorder_print(0, sentinel->right);
 }
 
+// This method returns the size of our AVL Tree
 int AVLTree::Size() {
     return size;
 }
 
+// This method identifies if our AVL Tree is empty or not
 int AVLTree::Empty() {
     return (size == 0);
 }
 
+// This method will sort our AVL Tree in alphabetical order
 vector <void *> AVLTree::Sorted_Vector() {
     array.clear();
     recursive_make_vector(sentinel->right);
@@ -137,17 +162,71 @@ vector <void *> AVLTree::Sorted_Vector() {
 }
 
 void AVLTree::Check_Balance(AVLTNode *n) {
-//    if (Empty()) return;
+    n->height = (n->left->height > n->right->height) ? (n->left->height + 1) : (n->right->height + 1);
+    n = Rebalance(n);
 
     return;
 }
 
+// This method rebalances the tree if necessary to maintain AVL
+// Tree constraints
 AVLTNode *AVLTree::Rebalance(AVLTNode *n) {
-    AVLTNode *a;
-    a = new AVLTNode;
-    return a;
+    AVLTNode *lsub, *rsub, *llsub, *rrsub;
+    int lheight = 0, rheight = 0, lsheight = 0, rsheight = 0;
+
+    if (abs(n->left->height - n->right->height) < 1) return n;
+
+    // Check which rebalance case we have
+    lsub = n->left;
+    rsub = n->right;
+
+    // Left Subtree Heavy
+    if (lheight > rheight) {
+        llsub = lsub->left;
+        rrsub = lsub->right;
+        lsheight = llsub->height;
+        rsheight = rrsub->height;
+
+        // Zig Zig
+        if (lsheight > rsheight) {
+            Rotate(lsub);
+            Check_Balance(lsub->parent);
+            return lsub;
+        }
+        // Zig Zag
+        else {
+            Rotate(rrsub);
+            Rotate(rrsub);
+            Check_Balance(rrsub->parent);
+            return ;
+        }
+    }
+    // Right Subtree Heavy
+    else {
+        llsub = rsub->left;
+        rrsub = rsub->right;
+        lsheight = llsub->height;
+        rsheight = rrsub->height;
+
+        // Zig Zag
+        if (lsheight > rsheight) {
+            Rotate(llsub);
+            Rotate(llsub);
+            Check_Balance(llsub->parent);
+            return ;
+        }
+        // Zig Zig
+        else {
+            Rotate(rsub);
+            Check_Balance(rsub->parent);
+            return rsub;
+        }
+    }
+
+    return n;
 }
 
+// This method will rotate the AVL Tree about the specified node
 void AVLTree::Rotate(AVLTNode *n) {
     AVLTNode *parent, *child, *oldparent;
 
@@ -180,16 +259,23 @@ void AVLTree::Rotate(AVLTNode *n) {
     parent->parent = n;
     if (child != sentinel) child->parent = parent;
 
+    // Adjust heights
+    n->height--;
+    parent->height++;
+
     return;
 }
 
+// This method will recursively call every node in our tree In Order
 void AVLTree::recursive_inorder_print(int level, AVLTNode *n) {
     if (n == sentinel) return;
     recursive_inorder_print(level+2, n->right);
-    printf("%*s%s\n", level, "", n->key.c_str());
+    printf("%*s%s %s\n", level, "", n->height, n->key.c_str());
     recursive_inorder_print(level+2, n->left);
 }
 
+// This method will recursively call every node in our tree to create
+// a sorted vector
 void AVLTree::recursive_make_vector(AVLTNode *n) {
     if (n == sentinel) return;
     recursive_make_vector(n->left);
@@ -197,6 +283,7 @@ void AVLTree::recursive_make_vector(AVLTNode *n) {
     recursive_make_vector(n->right);
 }
 
+// This method will recursively call and destroy every node in our AVL Tree
 void AVLTree::recursive_destroy(AVLTNode *n) {
     if (n == sentinel) return;
     recursive_destroy(n->left);
